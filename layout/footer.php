7f608ffb6840aba2f40ecfe8b922a6cbe2f03814
agusbudiprastono <prazt9@gmail.com>
2017-09-16 22:20:43 +0700

    v1

---
+++ layout/footer.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SESSION['status']=='admin'){
+?>
+  &copy; 2014 Aplikasi Penjualan Mini Market<br />
+  Designed and developed by Agus Budi Prastono. All rights reserved<br />
+  site : <a href="http://www.facebook.com" target="_blank">www.prazt9.com</a>
+<?php
+}
+?>
